c06a78e44c75cf87d2a4131dc17cb6ba6facc104
Huex <tohuex@gmail.com>
2018-08-26 16:42:29 +0300

    add Pushover controller

---
+++ PushoverTest/Controllers/PushoverController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Specialized;
+using System.Net;
+
+namespace PushoverTest.Controllers
+{
+    [Route("api/[controller]")]
+    public class PushoverController : Controller
+    {
+        // POST api/pushover
+        [HttpPost]
+        public void Post([FromBody] PushoverMessage value) =>
+            new WebClient().UploadValues("https://api.pushover.net/1/messages.json",
+                new NameValueCollection {
+                    { "token", value.Token },
+                    { "user", value.User },
+                    { "message", value.Message } });
+    }
+}
---
+++ PushoverTest/PushoverMessage.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System.Runtime.Serialization;
+
+namespace PushoverTest.Controllers
+{
+    public class PushoverMessage
+    {
+        /// <summary>
+        /// Gets or Sets Token
+        /// </summary>
+        [DataMember(Name = "token", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "token")]
+        public string Token { get; set; }
+
+        /// <summary>
+        /// Gets or Sets User
+        /// </summary>
+        [DataMember(Name = "user", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "user")]
+        public string User { get; set; }
+
+        /// <summary>
+        /// Gets or Sets Message
+        /// </summary>
+        [DataMember(Name = "message", EmitDefaultValue = false)]
+        [JsonProperty(PropertyName = "message")]
+        public string Message { get; set; }
+    }
+}
